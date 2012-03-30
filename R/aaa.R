210f7966ac91a9eb23e42dbb7491a69dc47b32b2
Martin Maechler <maechler@stat.math.ethz.ch>
2012-03-30 00:00:00 +0000

    version 0.8-1

---
+++ R/aaa.R
@@ -0,0 +1,5 @@
+.onLoad <- function(libname, pkgname)
+{
+    warning("The package 'nacopula' has been merged with package 'copula'.\n",
+            "Please now attach/load 'copula' instead of 'nacopula'.")
+}
